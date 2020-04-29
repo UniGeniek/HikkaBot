b39784640b23ca9f6ebef388fb78e5a64fc0d565
UniObito <58523638+UniObito@users.noreply.github.com>
2020-04-29 20:35:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ bot.on("ready", function () {
 
 // Команды
 const commands = {
-	"help": {
+	"v!help": {
 		process: function (msg, suffix, embed) {
 			const list = ["Bot - Бот",
 			"** Тег команды: "+config.prefix+" **",
